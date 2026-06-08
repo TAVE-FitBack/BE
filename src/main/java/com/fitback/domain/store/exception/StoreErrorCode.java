bbf5a06ba3101d61eaa4a78e8714f16f0157163f
7iniyo <  >
2026-06-08 21:02:37 +0900

    feat: 매장 초기 설정 API 구현

---
+++ src/main/java/com/fitback/domain/store/exception/StoreErrorCode.java
@@ -0,0 +1,19 @@
+package com.fitback.domain.store.exception;
+
+import com.fitback.global.exception.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum StoreErrorCode implements BaseErrorCode {
+
+    STORE_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
+            "매장을 찾을 수 없습니다."
+    );
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
