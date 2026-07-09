ef383b07fe8949b09e9dadc35e728c824d902625
7iniyo <  >
2026-07-09 12:47:43 +0900

    feat: CORS localhost:5173 추가 설정

--- src/main/java/com/fitback/global/config/SecurityConfig.java
+++ src/main/java/com/fitback/global/config/SecurityConfig.java
@@ -52,8 +52,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "http://localhost:5173"
         ));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
